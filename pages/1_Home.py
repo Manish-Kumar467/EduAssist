d160c5f7bb9c01b52f8150e0810dfed9bd55434b
pavan-k1 <pa23kum45@gmail.com>
2025-11-21 15:18:37 +0530

    login

--- pages/1_Home.py
+++ pages/1_Home.py
@@ -7,12 +7,12 @@ col1, col2 = st.columns(2)
 
 with col1:
     if st.button("📝 Summarisation Module"):
-        st.switch_page("2_Summarization.py")
+        st.switch_page("2_Summarization")
 
 with col2:
     if st.button("❓ Question Generation"):
-        st.switch_page("3_Question_Generation.py")
+        st.switch_page("3_Question_Generation")
 
 if st.button("🚪 Logout"):
     st.session_state["logged_in"] = False
-    st.switch_page("streamlit_app.py")
+    st.switch_page("streamlit_app")
